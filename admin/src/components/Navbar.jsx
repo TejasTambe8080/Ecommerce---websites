2b0f5f30e8d02016e30eb84e7396b320d2f96e9b
unknown <tejastambe683@gmail.com>
2026-02-04 09:39:25 +0530

    fix: Add .env.example to gitignore, improve admin navbar logo styling

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { assets } from '../assets/assets'
 
 const Navbar = ({ setToken }) => {
 return (
-<div className='flex items-center justify-between px-4'>
-<img src={assets.logo} alt="" />
-<button onClick={()=>setToken('')}>Logout</button>
+<div className='flex items-center justify-between px-4 py-2'>
+<img src={assets.logo} alt="Cartiva Admin" className='w-36 max-h-14 object-contain' />
+<button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
 </div>
 )
 }
